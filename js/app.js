36df47afbad33496b0c83c5acf2985c7481d259d
JabedH <87475551+JabedH@users.noreply.github.com>
2022-03-04 21:50:30 +0600

    add

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const API_KEY = `84c44ad1cc9065e8dfda92173a043d9c`;
 const gitCityName = () => {
   const cityName = document.getElementById("nameValue");
   const cityNameValue = cityName.value;
+  cityName.value = "";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameValue}&appid=${API_KEY}&units=metric`;
   console.log(url);
   fetch(url)
@@ -14,6 +15,10 @@ const setInnerText = (id, text) => {
 const displayTemperature = (temperature) => {
   console.log(temperature);
   setInnerText("cityName", temperature.name);
-  setInnerText("temp", temperature.main.temp);
+  setInnerText("temp", Math.ceil(temperature.main.temp));
   setInnerText("condition", temperature.weather[0].main);
+  setInnerText("condition", temperature.weather[0].main);
+  const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weatherIcon");
+  imgIcon.setAttribute("src", url);
 };
